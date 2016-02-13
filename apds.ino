36bf5f25968339dc1a25a49f049ff6890ba6db63
Benjamin P <benjamin.ab.art@gmail.com>
2016-02-13 17:21:24 +0100

    Create apds.ino

---
+++ apds.ino
@@ -0,0 +1,34 @@
+void apds_init()
+{
+   /*
+    INIT of APDS module
+  */
+  
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Start running the APDS-9960 light sensor (no interrupts)
+  if ( apds.enableLightSensor(false) ) {
+    Serial.println(F("Light sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during light sensor init!"));
+  }
+  
+  // Adjust the Proximity sensor gain
+  if ( !apds.setProximityGain(PGAIN_2X) ) {
+    Serial.println(F("Something went wrong trying to set PGAIN"));
+  }
+  
+  // Start running the APDS-9960 proximity sensor (no interrupts)
+  if ( apds.enableProximitySensor(false) ) {
+    Serial.println(F("Proximity sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during sensor init!"));
+  }
+  
+  // Wait for initialization and calibration to finish
+  delay(500);
+}
